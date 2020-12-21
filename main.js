7f534b027e710fb70712bb38de28a9f561bfc5bc
Steve Rydz <steverydz@gmail.com>
2020-12-21 20:11:23 +0000

    Remove menu bar

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ const { app, BrowserWindow } = require("electron");
 app.on("ready", () => {
   const win = new BrowserWindow();
   win.maximize();
+  win.setMenu(null);
   win.loadURL("https://notion.so");
 });
