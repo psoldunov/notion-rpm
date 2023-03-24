a1431366d12ba96cf494f53a298d18e9e236ae9e
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2023-03-25 01:15:58 +0200

    added window properties

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const { app, BrowserWindow, shell } = require("electron");
+const { app, BrowserWindow, shell } = require('electron');
 
-app.on("ready", () => {
-  const win = new BrowserWindow({
-    autoHideMenuBar: true,
-  });
-  win.maximize();
-  win.loadURL("https://notion.so");
+app.on('ready', () => {
+	const win = new BrowserWindow({
+		autoHideMenuBar: true,
+		width: 1920,
+		height: 1080,
+		minWidth: 800, // Set the minimum width to 600
+		minHeight: 600, // Set the minimum height to 400
+	});
+	win.maximize();
+	win.loadURL('https://notion.so');
 
-  // Deal with external links
-  win.webContents.setWindowOpenHandler(({ url }) => {
-    if (!url.startsWith("https://www.notion.so")) {
-      shell.openExternal(url);
-      return { action: "deny" };
-    }
-    return { action: "allow" };
-  });
+	// Deal with external links
+	win.webContents.setWindowOpenHandler(({ url }) => {
+		if (!url.startsWith('https://www.notion.so')) {
+			shell.openExternal(url);
+			return { action: 'deny' };
+		}
+		return { action: 'allow' };
+	});
 });
